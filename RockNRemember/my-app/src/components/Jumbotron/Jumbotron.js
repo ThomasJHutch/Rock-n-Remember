6df1a7405ab667c9d80ef228585737922b212dad
Tom Hutcheson <thomasjhutch@gmail.com>
2020-04-24 15:26:59 -0500

    created game and added all features

---
+++ RockNRemember/my-app/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+            <h1 className="display-4">Rock 'n' Remember</h1>
+            <p className="lead">Click on any image to get Rock points, but don't click on one more than once!</p>
+            <p className="lead">If you do... It's a long way to the top, if you wanna Rock 'n' Roll.</p>
+        </div>
+    </div>
+);
+
+export default Jumbotron;
